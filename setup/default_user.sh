03c0c9f168c6620caaf977daa6ce6f550dc486be
deeenes <turei.denes@gmail.com>
2022-07-04 20:05:53 +0200

    `default_user.sh`: from rocker/rstudio

---
+++ setup/default_user.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+DEFAULT_USER=${1:-${DEFAULT_USER:-"omnipath"}}
+
+if id -u "${DEFAULT_USER}" >/dev/null 2>&1; then
+    echo "User ${DEFAULT_USER} already exists"
+else
+    useradd -s /bin/bash -m "$DEFAULT_USER"
+    echo "${DEFAULT_USER}:${DEFAULT_USER}" | chpasswd
