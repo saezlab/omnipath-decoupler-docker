9eff6e75691b11c65c5703714bfd872514b9d4bf
deeenes <turei.denes@gmail.com>
2022-07-04 18:49:24 +0200

    `dependencies.sh`, modified from saezverse

---
+++ setup/dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# For building C-Python bindings
+apt -y install libcurl4-openssl-dev python3-dev
+# Extras for pypath
+apt -y install python3-igraph libcairo2-dev pkg-config
+
+
+# Python tools dependencies (unstated dependencies?)
+pip3 install --user --no-warn-script-location bioservices numpy scipy statsmodels pycurl pycairo
