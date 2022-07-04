079051d35790e1af401747a85356478e45502c8d
deeenes <turei.denes@gmail.com>
2022-07-04 23:16:40 +0200

    installing `decoupler` from git

--- setup/installpy.sh
+++ setup/installpy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Saez Python tools in PyPI
-pip3 install --user --no-warn-script-location pypath-omnipath omnipath decoupler
+pip3 install --user --no-warn-script-location pypath-omnipath omnipath
+pip3 install --user --no-warn-script-location git+https://github.com/saezlab/decoupler-py.git
 # Scanpy
 pip3 install 'scanpy[leiden]'
