69f050df302ef3aaaa91bafae71aac36ea6b9932
deeenes <turei.denes@gmail.com>
2022-07-04 23:15:48 +0200

    using `apt-get` and installing `pip`

--- setup/dependencies.sh
+++ setup/dependencies.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+DEBIAN_FRONTEND=noninteractive
+
 # For building C-Python bindings
-apt -y install libcurl4-openssl-dev python3-dev
+apt-get -y install libcurl4-openssl-dev libssl-dev python3 python3-dev python3-pip
 # Extras for pypath
-apt -y install python3-igraph libcairo2-dev pkg-config
+apt-get -y install python3-igraph libcairo2-dev pkg-config
 
 
 # Python tools dependencies (unstated dependencies?)
