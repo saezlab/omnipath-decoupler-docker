693fac8f7ac64748cb6789cf9794607de98ede4f
deeenes <turei.denes@gmail.com>
2022-07-05 16:11:47 +0200

    fixed pypath build issue due to submodule/path dependency

--- setup/installpy.sh
+++ setup/installpy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Saez Python tools in PyPI
-pip3 install --no-warn-script-location pypath-omnipath omnipath
+pip3 install --no-warn-script-location omnipath
+pip3 install --no-warn-script-location git+https://github.com/saezlab/pypath.git@nobiocypher
 pip3 install --no-warn-script-location git+https://github.com/saezlab/decoupler-py.git
 # Scanpy
 pip3 install --no-warn-script-location packaging 'scanpy[leiden]'
