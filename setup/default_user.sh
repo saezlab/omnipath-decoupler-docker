677004f2aac387137a45160a27679aff00360d2f
deeenes <turei.denes@gmail.com>
2022-07-05 13:33:21 +0200

    setting password in `default_user.sh`

--- setup/default_user.sh
+++ setup/default_user.sh
@@ -2,10 +2,11 @@
 set -e
 
 DEFAULT_USER=${1:-${DEFAULT_USER:-"omnipath"}}
+PASSWORD=${PASSWORD:-"szeged2022"}
 
 if id -u "${DEFAULT_USER}" >/dev/null 2>&1; then
     echo "User ${DEFAULT_USER} already exists"
 else
-    useradd -s /bin/bash -m "$DEFAULT_USER"
-    echo "${DEFAULT_USER}:${DEFAULT_USER}" | chpasswd
+    useradd -m -s /bin/bash -u 1000 -p "$(openssl passwd -1 -stdin <<< $PASSWORD)" "$DEFAULT_USER"
+    echo "${DEFAULT_USER}:${PASSWORD}" | chpasswd
 fi
