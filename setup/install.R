86752cb032b446773ef594b8229618e480b042c9
deeenes <turei.denes@gmail.com>
2022-07-05 13:34:35 +0200

    install `colorout` for R

--- setup/install.R
+++ setup/install.R
@@ -5,3 +5,4 @@ install.packages("devtools", repos = "https://cloud.r-project.org/")
 library(devtools)
 
 devtools::install_github("saezlab/OmnipathR")
+devtools::install_github("jalvesaq/colorout")
