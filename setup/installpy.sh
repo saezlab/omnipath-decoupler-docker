8bc6e057a56d71aea9bccbab21cf5bd230dcd94b
deeenes <turei.denes@gmail.com>
2022-07-05 13:35:11 +0200

    install Python packages directly to system path

--- setup/installpy.sh
+++ setup/installpy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Saez Python tools in PyPI
-pip3 install --user --no-warn-script-location pypath-omnipath omnipath
-pip3 install --user --no-warn-script-location git+https://github.com/saezlab/decoupler-py.git
+pip3 install --no-warn-script-location pypath-omnipath omnipath
+pip3 install --no-warn-script-location git+https://github.com/saezlab/decoupler-py.git
 # Scanpy
-pip3 install 'scanpy[leiden]'
+pip3 install --no-warn-script-location packaging 'scanpy[leiden]'
